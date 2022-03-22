02446851c36a7405b765681783cf53d157d41087
it1369 <gckim@gsitm.com>
2022-03-22 10:50:17 +0900

    proxy pattern

---
+++ src/test/java/com/bumblebee/designpattern/proxy/CalculatorServiceTest.java
@@ -0,0 +1,44 @@
+package com.bumblebee.designpattern.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CalculatorServiceTest {
+
+    @Test
+    void calculatorService() {
+
+        Calculator calculatorService = new CalculatorService();
+
+        int left = 10000;
+        int right = 5000;
+
+        int plusResult = calculatorService.plus(left, right);
+        int subtractResult = calculatorService.subtract(left, right);
+
+        assertEquals(plusResult, left+right);
+        assertEquals(subtractResult, left-right);
+
+    }
+
+    @Test
+    void calculatorServiceProxy() {
+
+        Calculator calculatorServiceProxy = new CalculatorServiceProxy();
+
+        int left = 10000;
+        int right = 5000;
+
+        int plusResult = calculatorServiceProxy.plus(left, right);
+        int subtractResult = calculatorServiceProxy.subtract(left, right);
+
+        assertEquals(plusResult, left+right);
+        assertEquals(subtractResult, left-right);
+
+    }
+
+
+}
